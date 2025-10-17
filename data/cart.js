de7d9fbb07606d2da89cec6868d41805e8a7c17e
Vigneshwaran <vigneshwaranr964@gmail.com>
2025-10-17 08:39:16 +0530

    generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,16 @@
-export let cart = [];
+// svae the date
+export let cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+  },
+];
 
+// add to cart function for product.js
 export function addToCart(productId) {
   let selectQuantity = document.querySelector(
     `.js-quantity-selector-${productId}`
